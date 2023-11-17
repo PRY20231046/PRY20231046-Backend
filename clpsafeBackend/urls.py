be22dec9afcd6c0f7af51ec47c29a9af2727ae1d
Diego Alonso Sanchez <u201612784@upc.edu.pe>
2023-11-17 00:20:38 -0500

    fdsf

--- clpsafeBackend/urls.py
+++ clpsafeBackend/urls.py
@@ -7,7 +7,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls'))
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] +static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
