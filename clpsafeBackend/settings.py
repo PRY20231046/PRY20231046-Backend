3e1990e3624cce048b1bca673819d57492f79ee7
Diego Alonso Sanchez <u201612784@upc.edu.pe>
2023-11-16 23:52:27 -0500

    hhhhh

--- clpsafeBackend/settings.py
+++ clpsafeBackend/settings.py
@@ -124,22 +124,17 @@ USE_I18N = True
 
 USE_TZ = True
 
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.2/howto/static-files/
 STATIC_ROOT= os.path.join(BASE_DIR, 'staticfiles')
 STATIC_TMP= os.path.join(BASE_DIR,'static')
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 os.makedirs(STATIC_TMP, exist_ok=True)
 os.makedirs(STATIC_ROOT, exist_ok=True)
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
 STATICFILES_STORAGE='whitenoise.storage.CompressedManifestStaticFilesStorage'
-
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
