47a5af8794af2ceb0634008fb98be9b849a934a7
Diego Alonso Sanchez <u201612784@upc.edu.pe>
2023-11-17 16:02:47 -0500

    final 2

--- api/urls.py
+++ api/urls.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
 from django.urls import path
-from .views import VehiculoView, DenunciaView
+from .views import VehiculoView, DenunciaView, ReporteView
 
 urlpatterns = [
     path('vehiculos/', VehiculoView.as_view(), name='vehiculos_list'),
     path('vehiculos/<int:id>', VehiculoView.as_view(), name='vehiculos_process'),
-    path('denuncias/', DenunciaView.as_view(), name='vehiculos_process'),
-    path('denuncias/<int:id>', DenunciaView.as_view(), name='vehiculos_process'),
+    path('denuncias/', DenunciaView.as_view(), name='denuncias_process'),
+    path('denuncias/<int:id>', DenunciaView.as_view(), name='denuncias_process'),
+    path('reportes/', ReporteView.as_view(), name='reportes_process'),
+    path('reportes/<int:id>', ReporteView.as_view(), name='reportes')
     #path('placas/', ReconocimientoView.as_view(), name='placa_process'),
 ]
